3337a8910b59f5a837bedfe6f31a7139fbe7fa97
nt03459 <nikhil.thorat@stud.th-deg.de>
2026-06-26 13:27:55 +0200

    updates

--- server/middlewares/exception_handlers.py
+++ server/middlewares/exception_handlers.py
@@ -1,13 +1,11 @@
-from fastAPI import Request
-
-from fastAPI.responses import JSONResponse
-
+from fastapi import Request
+from fastapi.responses import JSONResponse
 from logger import logger
 
-async def catch_exception_middleware(request : Request,call_next):
-    try: 
+
+async def catch_exception_middleware(request:Request,call_next):
+    try:
         return await call_next(request)
     except Exception as exc:
         logger.exception("UNHANDLED EXCEPTION")
-        return JSONResponse(status_code = 500,contents={"error": str(exc)})
-    
+        return JSONResponse(status_code=500,content={"error":str(exc)})
